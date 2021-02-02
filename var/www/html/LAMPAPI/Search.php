6fa5ac4d6b1c9b55b2cbbc0cd047bac7e0ffed91
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-02-02 12:06:23 -0500

    Fixed problem with string concatentation

--- var/www/html/LAMPAPI/Search.php
+++ var/www/html/LAMPAPI/Search.php
@@ -34,25 +34,24 @@ function getRequestInfo()
 }
 
 function buildQuery($data) {
-    $sql = "SELECT * FROM contact WHERE `user_id`="+$data["user_ID"] + " AND ";
+    $sql = "SELECT * FROM contact WHERE `user_id`=" . $data["user_ID"] . " AND ";
     switch($data["search_field"]) {
         case "First Name":
-            $sql = $sql + "`FirstName` LIKE '%" + $data["search_criteria"] + "%';";
+            $sql = $sql . "`FirstName` LIKE '%" . $data["search_criteria"] . "%';";
             break;
         case "Last Name":
-            $sql = $sql + "`LastName` LIKE '%" + $data["search_criteria"] + "%';";
+            $sql = $sql . "`LastName` LIKE '%" . $data["search_criteria"] . "%';";
             break;
         case "Phone":
-            $sql = $sql + "`PhoneNumber` LIKE '%" + $data["search_criteria"] + "%';";
+            $sql = $sql . "`PhoneNumber` LIKE '%" . $data["search_criteria"] . "%';";
             break;
         case "Email":
-            $sql = $sql + "`Email` LIKE '%" + $data["search_criteria"] + "%';";
+            $sql = $sql . "`Email` LIKE '%" . $data["search_criteria"] . "%';";
             break;
         case "Address":
-            $sql = $sql + "`StreetAddress` LIKE '%" + $data["search_criteria"] + "%';";
+            $sql = $sql . "`StreetAddress` LIKE '%" . $data["search_criteria"] . "%';";
             break;
     }
-    echo($sql);
     return $sql;
 }
 
@@ -69,7 +68,7 @@ function returnRows($result)
         $retValue = $retValue + sprintf('%d:{"FirstName":"%s","LastName":%s,"PhoneNumber":"%s", "Email":"%s", "Address":"%s %s, %s %s"},',
          $row["ID"], $row["FirstName"], $row["LastName"], $row["PhoneNumber"], $row["Email"], $row["StreetAddress"], $row["City"], $row["State"], $row["ZIP_Code"]);
     }
-    $retValue += '}';
+    $retValue .= '}';
 	sendResultInfoAsJson( $retValue );
 }
 
