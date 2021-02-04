7c26309eecd343377eee584ac175ec025a6552e7
Nicholas Chitty <nicholas.chitty@gmail.com>
2021-02-04 11:30:16 -0500

    Added result to doSearch funciton

--- var/www/html/js/code.js
+++ var/www/html/js/code.js
@@ -5,8 +5,7 @@ var userId = 0;
 var firstName = "";
 var lastName = "";
 
-function doLogin()
-{
+function doLogin() {
     //Get data from form
     var login = document.getElementById("loginName").value;
     var password = document.getElementById("loginPassword").value;
@@ -15,45 +14,41 @@ function doLogin()
 
     //Send data to php script
     var jsonPayLoad = '{"login" : "' + login + '", "password" : "' + password + '"}';
-    var url = urlBase + '/Login.'+extension;
+    var url = urlBase + '/Login.' + extension;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
     //What am I expecting to get back
-    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-    try
-    {
+    try {
         //sent
         xhr.send(jsonPayLoad);
-        
+
         //Prepare the response
         var jsonObject = JSON.parse(xhr.responseText);
         userId = jsonObject.id;
 
-        if(userId < 1)
-        {
+        if (userId < 1) {
             document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
+            return;
         }
 
         firstName = jsonObject.firstName;
-		lastName = jsonObject.lastName;
-        
+        lastName = jsonObject.lastName;
+
         setCookie("userID", userId);
         setCookie("firstName", firstName);
         setCookie("lastName", lastName);
 
         //where to send them to after they are authenticated
         window.location.href = "home.html";
-    } 
-    catch (err)
-    {
+    }
+    catch (err) {
         document.getElementById("loginResult").innerHTML = err.message;
     }
 }
 
-function doRegister()
-{
+function doRegister() {
     const firstName = document.getElementById("Firstname").value;
     const lastName = document.getElementById("Lastname").value;
     const email = document.getElementById("Email").value;
@@ -74,21 +69,19 @@ function doRegister()
 
     xhr.open("POST", url, false);
     // What we expect to recieve back
-    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-    try
-    {
+    try {
         // Send request
         xhr.send(jsonPayLoad);
-        
+
         // Need to check if registering worked.
         const jsonObject = JSON.parse(xhr.responseText);
 
         // Redirect
         window.location.href = "index.html"; // Send back to login screen.
     }
-    catch(err)
-    {
+    catch (err) {
         document.getElementById("registerResult").innerHTML = err.message;
     }
 }
@@ -116,13 +109,12 @@ function addContact() {
 
     xhr.open("POST", url, false);
     // What we expect to recieve back
-    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-    try
-    {
+    try {
         // Send request
         xhr.send(jsonPayLoad);
-        
+
         // Need to check if registering worked.
         const jsonObject = JSON.parse(xhr.responseText);
 
@@ -130,8 +122,7 @@ function addContact() {
         // Redirect
         window.location.href = "home.html"; // Send back to home screen.
     }
-    catch(err)
-    {
+    catch (err) {
         document.getElementById("addResult").innerHTML = err.message;
     }
 }
@@ -150,25 +141,22 @@ function deleteContact() {
 
     xhr.open("POST", url, false);
     // What we expect to recieve back
-    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-    try
-    {
+    try {
         // Send request
         xhr.send(jsonPayLoad);
-        
+
         // Need to check if registering worked.
         const jsonObject = JSON.parse(xhr.responseText);
 
     }
-    catch(err)
-    {
+    catch (err) {
         document.getElementById("registerResult").innerHTML = err.message;
     }
 }
 
-function doSearch()
-{
+function doSearch() {
     loadCookie();
     const dropDown = document.getElementsByClassName("selectSearchbar")[0];
     const index = dropDown.selectedIndex;
@@ -176,49 +164,56 @@ function doSearch()
     const term = document.getElementById("searchBox").value;
 
     let jsonPayLoad = '{"user_ID": ' + userId + '"search_field" : "' + tag + '", "search_criteria" : "' + term + '"}';
+    const url = urlBase + '/Search.' + extension;
+    const xhr = new XMLHttpRequest();
 
-    const names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    const email = ['a@mail.com', 'b@mail.com', 'c@mail.com', 'd@mail.com', 'e@mail.com', 'f@mail.com', 'g@mail.com', 'h@mail.com', 'i@mail.com', 'j@mail.com'];
-    const number = ['407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234', '407-555-1234'];
-    const address = ['123 Street St', '123 Street St', '123 Street St', '123 Street St', '123 Street St', '123 Street St', '123 Street St', '123 Street St', '123 Street St', '123 Street St'];
+    xhr.open("POST", url, false);
+    // What we expect to recieve back
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    
+    try {
+        xhr.send(jsonPayLoad);
+        const jsonObject = JSON.parse(xhr.responseText);
 
+        let resultArea = document.getElementsByClassName("search-results")[0];
+        // Build search result cards
+        // this is a temporary loop, will loop through json responses
+        // believe me, this could of been way worse
+        let i = 0;
+        while (jsonObject["results"].length >= 0) {
+            let result = JSON.parse(jsonObject["results"].shift());
+            resultArea.innerHTML += '<div class="search-result" id=' + i + '>' +
+                '<div class="row">' +
+                ' <div class="col-8 container-fluid card">' +
+                '<h1 class="card-title me-auto contact-name" id="contact-name">' +
+                result["FirstName"] + " " + result["LastName"] +
+                '</h1>' +
+                '<div class="row">' +
+                '<div class="col-6 text-start" id="email">' + result["Email"] + '</div>' +
+                '<div class="col-6 text-end" id="phone number">' + result["PhoneNumber"] + '</div>' +
+                '</div>' +
+                '<div class="row">' +
+                '<p class="me-auto" id="address">' + result["Address"] + '</p>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-4 container-fluid button-area">' +
+                '<span class="align-top">' +
+                '<button class="contact-button btn btn-info" onClick = "doUpdate(' + i + ');">✏</button>' +
+                '</span>' +
+                '<span class="align-bottom">' +
+                '<button class="contact-button btn btn-danger" onClick = "doDelete(' + i + ');">✖</button>' +
+                ' </span>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+                i++;
+        }
+    } catch (error) {
 
-    let resultArea = document.getElementsByClassName("search-results")[0];
-
-    // Build search result cards
-    // this is a temporary loop, will loop through json responses
-    // believe me, this could of been way worse
-    for(let i = 0; i < 10; i++)
-    {
-       resultArea.innerHTML += '<div class="search-result" id=' + i + '>' + 
-                                    '<div class="row">' + 
-                                        ' <div class="col-8 container-fluid card">' + 
-                                            '<h1 class="card-title me-auto contact-name" id="contact-name">' + 
-                                               names[i] + 
-                                            '</h1>' + 
-                                            '<div class="row">' + 
-                                                '<div class="col-6 text-start" id="email">' + email[i] + '</div>' + 
-                                                '<div class="col-6 text-end" id="phone number">' + number[i] + '</div>' + 
-                                            '</div>' + 
-                                            '<div class="row">' + 
-                                                '<p class="me-auto" id="address">' + address[i] + '</p>' + 
-                                            '</div>' + 
-                                        '</div>' + 
-                                        '<div class="col-4 container-fluid button-area">' + 
-                                            '<span class="align-top">' +
-                                                '<button class="contact-button btn btn-info" onClick = "doUpdate(' + i + ');">✏</button>' + 
-                                            '</span>' + 
-                                            '<span class="align-bottom">' + 
-                                                '<button class="contact-button btn btn-danger" onClick = "doDelete(' + i + ');">✖</button>' +
-                                            ' </span>' + 
-                                        '</div>' + 
-                                    '</div>' +
-                                '</div>';
     }
 }
 
-function doLogout()
-{
+function doLogout() {
     userId = 0;
     firstName = "";
     lastName = "";
@@ -228,20 +223,20 @@ function doLogout()
 
 function setCookie(propertyName, propertyValue) {
     var d = new Date();
-    d.setTime(d.getTime + 1000*60*20);
-    var expires = "expires="+d.toUTCString();
+    d.setTime(d.getTime + 1000 * 60 * 20);
+    var expires = "expires=" + d.toUTCString();
     document.cookie = propertyName + "=" + propertyValue + ";" + expires + ";path=/";
 }
 
 function getCookie(propertyName) {
     var name = propertyName + "=";
     var propertyArray = document.cookie.split(";");
-    for(var i = 0; i< propertyArray.length; i++) {
+    for (var i = 0; i < propertyArray.length; i++) {
         var property = propertyArray[i];
-        while(property.charAt(0) == ' ') {
+        while (property.charAt(0) == ' ') {
             property = property.substring(1);
         }
-        if(property.indexOf(name) == 0) {
+        if (property.indexOf(name) == 0) {
             return property.substring(name.length, property.length);
         }
     }
@@ -250,7 +245,7 @@ function getCookie(propertyName) {
 
 function loadCookie() {
     var user_ID = getCookie("userID");
-    if(user_ID != "") {
+    if (user_ID != "") {
         userId = user_ID;
         firstName = getCookie("firstName");
         lastName = getCookie("lastName");
@@ -259,45 +254,39 @@ function loadCookie() {
         firstName = "";
         lastName = "";
     }
-    
+
 }
 
 function deleteCookie() {
     document.cookie = "expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
-function goToAdd()
-{
+function goToAdd() {
     window.location.href = "add.html";
 }
 
-function doUpdate(id)
-{
+function doUpdate(id) {
     alert("PLACEHOLDER: update called for id: " + id);
 }
 
-function doDelete(id)
-{
+function doDelete(id) {
     alert("PLACEHOLDER: delete called for id: " + id);
 }
 
 
 // Allows for clicking enter to login
 let fileName = location.href.split("/").slice(-1);
-if(fileName == "index.html")
-{   
-    document.getElementById("login-div").addEventListener("keydown", function(e)
-    {
-        if(e.key == "Enter")
-        {
+if (fileName == "index.html") {
+    document.getElementById("login-div").addEventListener("keydown", function (e) {
+        if (e.key == "Enter") {
             const loginName = document.getElementById("loginName").value;
             const password = document.getElementById("loginPassword").value;
 
-            if(loginName != "" && password != "")
+            if (loginName != "" && password != "")
                 doLogin();
         }
     });
 }
 
-if(userId > 0)
+if (userId > 0)
     document.getElementById("loggedInAs").innerHTML += firstName + " " + lastName;
